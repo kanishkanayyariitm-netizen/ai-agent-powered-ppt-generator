ff558eccd0f773ca0976c357612d09a404d8a613
kanishkanayyariitm-netizen <kanishkanayyar.iitm@gmail.com>
2026-07-31 16:19:15 +0530

    Update kanishka_app.py

--- kanishka_app.py
+++ kanishka_app.py
@@ -106,7 +106,9 @@ if all(ALL_API) and user_query:
       if st.button("Clicks to generate:", key="generate_img_button"):
         with st.spinner("Running Agent.."):
           data=f"https://image.pollinations.ai/{user_query}"
-          time.sleep(3)
+          import requests as r
+          img_data = r.get(data)
+          #time.sleep(3)
           st.image(data)
           #st.image("image.jpeg")
           #st.return()
